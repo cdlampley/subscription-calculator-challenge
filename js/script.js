0747b20cb1a4f5120cfee6b7591174115e04d894
cdlampley <cdlampley@gmail.com>
2022-04-01 10:19:15 -0400

    Adding subscription-calculator-challenge to github

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Your JS code here
+var subTypeElement = document.querySelector(".subscription-select");
+var subDurationElement = document.querySelector(".duration-select");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+subTypeElement.addEventListener("change", function (e) {
+  subType = e.target.value;
+  updateSubscriptionDiv();
+  //console.log(subType);
+});
+
+subDurationElement.addEventListener("change", function (e) {
+  subDuration = Number(e.target.value);
+  updateSubscriptionDiv();
+  //console.log(subDuration);
+});
+
+var updateSubscriptionDiv = function () {
+  var monthlyCost = 5;
+  if (subType === "standard") {
+    monthlyCost = 7;
+  } else if (subType === "premium") {
+    monthlyCost = 10;
+  }
+  var total = subDuration * monthlyCost;
+  result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
